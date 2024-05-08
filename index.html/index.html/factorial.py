b4fd8e252d79362db9e6722992111bf979750a60
karthick-slg1949 <167311178+karthick-slg1949@users.noreply.github.com>
2024-05-07 22:42:58 -0700

    test

--- index.html/index.html/factorial.py
+++ index.html/index.html/factorial.py
@@ -1,7 +1,7 @@
 def factorial(n):
-  if n==0:
+  if n == 0:
     return 1
   else:
     return n * factorial(n-1)
-    num=int(input("Enter the number:"))
-    print("Factorial no of",num,"is",factorial(num))
+num=int(input("Enter a number:"))
+print("Factorial of",num,"is",factorial(num))
