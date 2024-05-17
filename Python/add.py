578ccc5f68f491fa9b95b076abceba489c4eaa0a
karthick-slg1949 <167311178+karthick-slg1949@users.noreply.github.com>
2024-05-17 00:02:56 -0700

    tst

--- Python/add.py
+++ Python/add.py
@@ -4,8 +4,8 @@ c = 0
 for i in range (1, n+1):
     t = n % i
     if t == 0:
-       c = c + 1
-if c <= 2:
+       c +=  1
+if c == 2:
     print("The number is prime no")
 else:
     print("The number is not prime no")
